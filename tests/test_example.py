0203f7db6c2817e3d62b0a5fe64cb1ce8c66532d
I G <ignevan@yandex.ru>
2022-01-02 16:08:32 +0500

    initial commit

---
+++ tests/test_example.py
@@ -0,0 +1,8 @@
+from hl_pytest.example import reverse
+
+def test_reverse():
+    assert reverse('Hexlet') == 'telxeH'
+
+def test_reverse_for_empty_string():
+    assert reverse('') == ''
+
---
+++ tests/test_hl_pytest.py
@@ -0,0 +1,5 @@
+from hl_pytest import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
